deeb0b53b67eaa9e718a396ba834c3df67a0acd6
Dang Thai Son <sondanguit@gmail.com>
2023-03-28 12:12:56 +0700

    Animation_CompleteApp

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.example.mylibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,8 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         adapter = new BookRecViewAdapter(this,"allBooks");
         booksRecView = findViewById(R.id.booksRecViews);
 
@@ -26,5 +30,17 @@ public class AllBooksActivity extends AppCompatActivity {
 
         ArrayList<Book> books = new ArrayList<>();
         adapter.setBooks(Utils.getAllBooks());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
